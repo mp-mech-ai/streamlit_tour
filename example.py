84a8b7c6b0799692d0dd2523c6f690a196e38424
Malo POTEREAU <malo.potereau@gmail.com>
2026-04-02 15:53:37 +0200

    Fixes the button disappearance when spam clicked

--- example.py
+++ example.py
@@ -13,25 +13,23 @@ st.text_input("Main area input", key="driver_step_1")
 with st.container(key="driver_step_2"):
     st.write("This is a paragraph. " * 10)
 
-if "tour_done" not in st.session_state:
-    st.session_state.tour_done = False
+if st.button("Start Tour"):
+    print("Starting tour")
+    result = Tour.start(
+        steps=[
+            Tour.bind("driver_step_1", title="Driver.js step 1", desc="This is a description", align="center"),
+            Tour.bind("driver_step_2", title="Driver.js step 2"),
+            Tour.bind("sidebar_btn", title="Driver.js step 3"),
+        ],
+        show_progress=True,
+        animate=True,
+        overlay_opacity=0.75,
+        one_time_tour=True,
+        key=f"presentation_tour",
+    )
 
-if not st.session_state.tour_done:
-    if st.button("Start Tour"):
-        result = Tour.start(
-            steps=[
-                Tour.bind("driver_step_1", title="Driver.js step 1", desc="This is a description", align="center"),
-                Tour.bind("driver_step_2", title="Driver.js step 2"),
-                Tour.bind("sidebar_btn", title="Driver.js step 3"),
-            ],
-            show_progress=True,
-            animate=True,
-            overlay_opacity=0.75,
-            one_time_tour=True,
-            key=f"presentation_tour",
-        )
-        
-        if result.get("finished") or result.get("skipped"):
-            st.session_state.tour_done = True
+    if result.get("finished") or result.get("skipped"):
+        print("Tour finished/skipped")
+        st.session_state.tour_done = True
 
     
